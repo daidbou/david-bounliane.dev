9aac7dc73710d3ca6a65b4fee1a82d08db66fd8a
David <89360632+daidbou@users.noreply.github.com>
2023-10-03 00:53:53 +0200

    Update About.jsx

--- components/About.jsx
+++ components/About.jsx
@@ -10,14 +10,21 @@ export const About = () => {
                     <p className='uppercase text-sm tracking-widest text-slate-400'>About</p>
                     <h2 className='py-4'>Who I Am</h2>
                     <p className='text-md text-slate-300'>
-                      Hello nice to meet you, my name is David I'm 21 years old and I'm in 4th year of CS engineering school at <strong><a href="https://www.utt.fr">l'Université de Technologie de Troyes</a></strong>. I like many things, like sports, photography, music, cooking but most importantly programming.
-                      
-                        I am seeking for a six-month internship starting from February 2023. I wish to deepen my knowledge in web development, both as front-end and back-end. <br/>I already know how to code in php and javascript, however I still have a superficial knowledge of frameworks built on these languages.
-                      
-                      A 6 months internship would be for me the opportunity to learn how to work in a team, master one or more frameworks like ReactJS, AngularJS or Laravel and especially gain experience and maturity in this domain. I am eager to learn web development.
+                      My name is David Bounliane, and I am currently in my fifth year of engineering studies of CS engineering school 
+                      at <strong><a href="https://www.utt.fr">l'Université de Technologie de Troyes</a></strong>. 
+                      From the moment I wrote my first line of code, I realized that software development wasn't just a field of study for me, but a passion. 
+                      The intricacies of algorithms, the challenges of debugging, and the satisfaction of seeing a working application come to life captivate me like nothing else.
+                      Over the course of my academic journey, I have not only acquired theoretical knowledge but also have dedicated countless hours to practical applications, 
+                      projects, and extracurricular activities that revolve around programming. 
+                      Each experience has only deepened my love for the domain and affirmed my belief that this is the path I want to tread on.
+                      Now, as I approach the culmination of my engineering program, I am eager to immerse myself further and refine my skills in a real-world setting. 
+                      I am seeking an internship opportunity that would allow me to be a part of innovative software development 
+                      projects and work alongside professionals who share the same zeal for technology as I do.
+                      I am confident that with my passion, combined with the academic foundation I have built, I can contribute positively to any team and project. 
+                      Furthermore, this internship will be invaluable in equipping me with the knowledge and experience necessary to excel in my future career in software development.
                       <br/>
                       <br/>
-                      if you are looking for a intern in web development, feel free to contact me on Linkedin : </p>
+                      if you are looking for a intern, feel free to contact me on Linkedin : </p>
                       <a href='' ><p id='copy' className=' text-blue-400'>linkedin.com/in/david-bounliane/</p></a>
                       <p className='text-md text-slate-300'>or at my email address : </p><a href=''><p id='copy1' className=' text-blue-400'>david.bounliane@gmail.com</p></a>
 
